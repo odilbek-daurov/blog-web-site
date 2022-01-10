d836bfa525fe3e1b5f17c9d37737a166af10c61a
odilbek-daurov <daurovodilbek@gmail.com>
2022-01-10 21:37:47 +0500

    admin update

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
+from django.contrib.admin.helpers import checkbox
+from django.db import models
 from .models import Category, Blog, Comment
-admin.site.register(Category)
-admin.site.register(Blog)
+
+@admin.register(Category)
+class BlogCategory(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+    list_display = ['name', 'slug']
+    
+
+@admin.register(Blog)
+class BlogClass(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('title',)}
+    list_display = ['title','slug']
+
+
 admin.site.register(Comment)
